8010e0dfef434b66cefacf7ccb7d1d44ae1c95f3
Immanuel Mundary <immanuelmundary09@gmail.com>
2023-06-25 15:46:43 +0530

    dev t

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ require("dotenv").config();
 
 const mongoString = process.env.DATABASE_URL;
 
-// mongoose.connect(mongoString);
-// const database = mongoose.connection;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
-// database.on("error", (error) => {
-//   console.log(error);
-// });
+database.on("error", (error) => {
+  console.log(error);
+});
 
-// database.once("connected", () => {
-//   console.log("Database Connected");
-// });
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 const app = express();
 
@@ -22,6 +22,11 @@ const routes = require("./routes/timetableRoutes");
 app.use(express.json());
 
 app.use("/api", routes);
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
 
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`);
